77e8cd0b870e1a4ebc1fdac4dad1bc2d96bd0eb0
Arpit Jain <arpitjain101992@gmail.com>
2021-08-12 13:26:17 +0530

    Logging, Exception handling and doc-strings added to comprehend router

--- datahub/sql/apis/routes/aws/comprehend_router.py
+++ datahub/sql/apis/routes/aws/comprehend_router.py
@@ -14,7 +14,9 @@ from sql.apis.schemas.responses.aws.comprehend_response import (
 from sql.controllers.aws.comprehend_controller import ComprehendController
 from fastapi.security import OAuth2PasswordBearer
 from commons.auth import decodeJWT
+from sql import logger
 
+logging = logger(__name__)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/login")
 comprehend_router = APIRouter()
 
@@ -27,20 +29,22 @@ def create_document_classifier(
     create_document_classifier_request: CreateDocumentClassifier,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to create document classifier using AWS Comprehend]
 
     Args:
-        create_document_classifier_request (CreateDocumentClassifier): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        create_document_classifier_request (CreateDocumentClassifier): [Create Document Classifier request expected by AWS Comprehend]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        e: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [CreateDocumentClassifierResponse]: [Document Classifier response from AWS Comprehend]
     """
     try:
+        logging.info("Calling /aws/comprehend/create_document_classifier endpoint")
+        logging.debug(f"Request: {create_document_classifier_request}")
         if decodeJWT(token=token):
             response = ComprehendController().create_document_classifier_controller(
                 create_document_classifier_request
@@ -52,8 +56,11 @@ def create_document_classifier(
                 detail="Invalid access token",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-    except Exception as e:
-        raise e
+    except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/create_document_classifier endpoint: {error}"
+        )
+        raise error
 
 
 @comprehend_router.post(
@@ -64,20 +71,24 @@ def stop_training_document_classifier(
     stop_training_document_classifier_request: DocumentClassifier,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to stop training process of document classifier using AWS Comprehend]
 
     Args:
-        stop_training_document_classifier_request (DocumentClassifier): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        stop_training_document_classifier_request (DocumentClassifier): [Stop Document classifier training request]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [DocumentClassifierResponse]: [Response from AWS Comprehend]
     """
     try:
+        logging.info(
+            "Calling /aws/comprehend/stop_training_document_classifier endpoint"
+        )
+        logging.debug(f"Request: {stop_training_document_classifier_request}")
         if decodeJWT(token=token):
             response = (
                 ComprehendController().stop_training_document_classifier_controller(
@@ -92,6 +103,9 @@ def stop_training_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/stop_training_document_classifier endpoint: {error}"
+        )
         raise error
 
 
@@ -103,20 +117,22 @@ def delete_document_classifier(
     delete_document_classifier_requeset: DocumentClassifier,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to delete document classifier using AWS Comprehend]
 
     Args:
-        delete_document_classifier_requeset (DocumentClassifier): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        delete_document_classifier_requeset (DocumentClassifier): [Delete Document classifier request]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [DocumentClassifierResponse]: [Response from AWS Comprehend]
     """
     try:
+        logging.info("Calling /aws/comprehend/delete_document_classifier endpoint")
+        logging.debug(f"Request: {delete_document_classifier_requeset}")
         if decodeJWT(token=token):
             response = ComprehendController().delete_document_classifier_controller(
                 delete_document_classifier_requeset
@@ -129,6 +145,9 @@ def delete_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/delete_document_classifier endpoint: {error}"
+        )
         raise error
 
 
@@ -137,20 +156,22 @@ def describe_document_classifier(
     describe_document_classifier_request: DocumentClassifier,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to describe document classifier using AWS Comprehend]
 
     Args:
-        describe_document_classifier_request (DocumentClassifier): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        describe_document_classifier_request (DocumentClassifier): [Describe Document classifier request]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [dict]: [Response with all the document classifier details]
     """
     try:
+        logging.info("Calling /aws/comprehend/describe_document_classifier endpoint")
+        logging.debug(f"Request: {describe_document_classifier_request}")
         if decodeJWT(token=token):
             response = ComprehendController().describe_document_classifier_controller(
                 describe_document_classifier_request
@@ -163,6 +184,9 @@ def describe_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/describe_document_classifier endpoint: {error}"
+        )
         raise error
 
 
@@ -170,19 +194,20 @@ def describe_document_classifier(
 def list_document_classifier(
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to list all document classifier using AWS Comprehend]
 
     Args:
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [dict]: [list of all the document classifiers]
     """
     try:
+        logging.info("Calling /aws/comprehend/list_document_classifier endpoint")
         if decodeJWT(token=token):
             response = ComprehendController().list_document_classifier_controller()
             return response
@@ -193,6 +218,9 @@ def list_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/list_document_classifier endpoint: {error}"
+        )
         raise error
 
 
@@ -203,20 +231,22 @@ def deploy_document_classifier(
     deploy_model_request: DeployModel,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to deploy a document classifier using AWS Comprehend]
 
     Args:
-        deploy_model_request (DeployModel): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        deploy_model_request (DeployModel): [Deploy AWS Comprehend classifier request]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [DeployModelResponse]: [Deployed model response]
     """
     try:
+        logging.info("Calling /aws/comprehend/deploy_document_classifier endpoint")
+        logging.debug(f"Request: {deploy_model_request}")
         if decodeJWT(token=token):
             response = ComprehendController().deploy_document_classifier_controller(
                 deploy_model_request
@@ -229,6 +259,9 @@ def deploy_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/deploy_document_classifier endpoint: {error}"
+        )
         raise error
 
 
@@ -239,20 +272,22 @@ def undeploy_document_classifier(
     undeploy_model_request: UnDeployModel,
     token: str = Depends(oauth2_scheme),
 ):
-    """[summary]
+    """[API router to undeploy a document classifier using AWS Comprehend]
 
     Args:
-        undeploy_model_request (UnDeployModel): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        undeploy_model_request (UnDeployModel): [Un-Deploy AWS Comprehend classifier request]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [UnDeployModelResponse]: [Un-Deployed model response]
     """
     try:
+        logging.info("Calling /aws/comprehend/undeploy_document_classifier endpoint")
+        logging.debug(f"Request: {undeploy_model_request}")
         if decodeJWT(token=token):
             response = ComprehendController().undeploy_document_classifier_controller(
                 undeploy_model_request
@@ -265,26 +300,31 @@ def undeploy_document_classifier(
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(
+            f"Error in /aws/comprehend/undeploy_document_classifier endpoint: {error}"
+        )
         raise error
 
 
 @comprehend_router.get("/aws/comprehend/get_predictions")
 def get_predictions(endpoint_arn: str, text: str, token: str = Depends(oauth2_scheme)):
-    """[summary]
+    """[API router to get predicts from a AWS Comprehend document classifier]
 
     Args:
-        endpoint_arn (str): [description]
-        text (str): [description]
-        token (str, optional): [description]. Defaults to Depends(oauth2_scheme).
+        endpoint_arn (str): [Deployed AWS Comprehend model endpoint]
+        text (str): [text to be classified]
+        token (str, optional): [Bearer token for authentication]. Defaults to Depends(oauth2_scheme).
 
     Raises:
-        HTTPException: [description]
-        error: [description]
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
 
     Returns:
-        [type]: [description]
+        [dict]: [Prediction results]
     """
     try:
+        logging.info("Calling /aws/comprehend/get_predictions endpoint")
+        logging.debug(f"Request: {endpoint_arn=},{text=}")
         if decodeJWT(token=token):
             return ComprehendController().get_predictions_controller(
                 endpoint_arn=endpoint_arn, text=text
@@ -296,4 +336,5 @@ def get_predictions(endpoint_arn: str, text: str, token: str = Depends(oauth2_sc
                 headers={"WWW-Authenticate": "Bearer"},
             )
     except Exception as error:
+        logging.error(f"Error in /aws/comprehend/get_predictions endpoint: {error}")
         raise error
